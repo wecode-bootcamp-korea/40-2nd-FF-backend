05b0b1b7a73b5528f5e17fac0858723bc368988f
YAE00 <yeallap00@gmai.com>
2023-01-05 15:09:11 +0900

    [40기 윤은애] ADD 메인페이지&상세페이지

--- api/routes/storeRouter.js
+++ api/routes/storeRouter.js
@@ -3,6 +3,10 @@ const { storeController } = require('../controllers')
 
 const router = express.Router();
 
-router.get('', storeController.getStoreList)
+router.get('', storeController.getAllStores)
+router.get('/moods/:moodId', storeController.getStoresByMood)
+router.get('/:storeId', storeController.getStoreById)
+router.get('/likes/:storeId', storeController.getStoreLikes)
+router.get('/reviews/:storeId', storeController.getReviewByStore)
 
 module.exports = router
